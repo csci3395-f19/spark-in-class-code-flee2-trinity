92779c5914eecb44066616ad56350c7b33aa841b
2017-08-23 <flee2@atlas00.cs.trinity.edu>
2019-10-04 23:54:28 -0500

    Done for sql

--- answers.sql
+++ answers.sql
@@ -1,14 +1,49 @@
 use BDA_VQ;
 /*Question 1*/
-SELECT COUNT(*)
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_TYPE = 'BASE TABLE';
+SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE';
 /*Question 2*/
 SELECT count(*) FROM users;
 /*Question 3*/
-
+set @mint = (SELECT MIN(TABLE_ROWS) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE');
+SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'and TABLE_ROWS = @mint;
 /*Question 4*/
 select count(correct_option) from multiple_choice_questions;
 /*Question 5*/
 select count(*) from quiz_course_close_assoc where courseid = 3;
+/*Question 6*/
+select count(*), courseid from quiz_course_close_assoc GROUP BY courseid;
+/*Question 7*/
+select MAX(question_id) from code_answers;/*26*/
+select DISTINCT quizid from code_answers where question_id = 26;/*53*/
+select courseid from quiz_course_close_assoc where quizid = 53;/*11, 12*/
+select * from courses where courseid = 11 or courseid = 12;
+/*Question 8*/
+set @funAll = (select count(distinct quizid, question_id, userid) from code_answers where question_type = 1);
+set @funC = (select count(distinct quizid, question_id, userid) from code_answers where question_type = 1 AND correct = 1);
+select @funC / @funAll; 
+/*Question 9*/
+set @qlambda = (select quizid from lambda_assoc);/*11*/
+select count(*) from quiz_course_close_assoc where quizid = @qlambda;
+/*Question 10*/
+set @maxC = (select max(total) from (select count(*) as total from code_answers where question_type = 1 AND correct = 1 GROUP BY userid) as result);
+select user from (select count(*) as total, userid as user 
+from code_answers where question_type = 1 AND correct = 1 GROUP BY userid) as result where total = @maxC;
+/*Question 11*/
+SELECT s_t FROM (SELECT count(*) AS ct, spec_type AS s_t 
+FROM variable_specifications GROUP BY spec_type) AS top3 ORDER BY ct DESC LIMIT 3;
+/*Question 12*/
+set @codeQ = (select count(distinct quizid) from code_answers);
+set @allQ = (select count(*) from quizzes);
+select @codeQ / @allQ;
+/*Question 13*/
+set @mulQ = (select count(*) from multiple_choice_assoc);
+select @mulQ/@allQ;
+/*Question 14*/
+SET @codeQQ = (SELECT COUNT(DISTINCT quizid, question_id) FROM  code_answers);
+SELECT @codeQQ/@allQ;
+/*Question 15*/
+SELECT MAX(attemps) from (SELECT COUNT(*) AS attemps FROM code_answers GROUP BY userid) as maxAttempts;
+
+
+
 
--- sql.md
+++ sql.md
@@ -1,26 +1,48 @@
 1. How many tables are in this database?
 * 16
+
 2. How many users are currently set up?
 * 222
-3. Which table has the smallest number of entries? How many entries does it have?
-??
+
+3. Which table has the smallest number of entries? How many entries does it have
+* lambda_assoc, lambda_questions, both have 1 row
+
 4. How many answers have been posted for multiple choice questions?
 * 108
+
 5. How many quizzes were given to section 6 of CSCI 1320 in Fall 2015?
 * 22
+
 6. Which course has the most quizzes? How many quizzes does it have?
 *  CSCI1320  F16 section 5, 26 quizzes
+
 7. Which course has the most questions on quizzes? How many questions does it have?
 * CSCI1321 F16 Section 1 and CSCI1320 F16 Section 5, 26 questions
+
 8. What fraction of students who submit an answer for a function question eventually get it right?
+* 0.6841
 
 9. How many courses have given questions of the "lambda" type?
+* 9
 
 10. Which user, by userid, has the most correct answers? How many do they have?
+* userid: 39
+* 15 correct answers
 
 11. What are the three most common spec_types for variables in the problems?
+* 0
+* 2
+* 3
 
 12. What fraction of quizzes have coding questions?
+* 0.3818
+
 13. What is the average number of multiple choice questions per quiz?
+* 2.0182
+
 14. What is the average number of coding questions per quiz?
+* 0.7636
+
 15. What is the most attempts per student for any course on any quiz?
+* 345
+
